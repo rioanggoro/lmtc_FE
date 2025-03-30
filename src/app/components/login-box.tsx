175079c1bdd56f9d483e2b5ff34e99a4356c0fdf
rioanggoro <rioanggoro37@gmail.com>
2025-03-31 01:04:17 +0700

    fix: in page login-box global css

--- src/app/components/login-box.tsx
+++ src/app/components/login-box.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LoginBox() {
   return (
-    <div className="login__box login__box--image relative hidden overflow-hidden bg-sky-400 md:block md:w-1/2">
+    <div className="login__box login__box--image relative hidden overflow-hidden bg-[radial-gradient(50%_50%_at_50%_50%,_#fff_0%,_#54bfff_90%)] md:block md:w-1/2">
       <div className="absolute inset-0">
         <span className="line-animation"></span>
         <span className="line-animation"></span>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,13 +22,7 @@ export default function Home() {
 
         <section className="login__content flex flex-1 flex-col px-8 py-4">
           <div className="login__form mx-auto w-full max-w-md">
-            <h1 className="login__title mb-2 text-center text-2xl font-bold text-gray-800">
-              Welcome to LMCT+!
-            </h1>
-            <p className="mb-8 text-center text-lg text-gray-600">
-              LMCT Plus is Australia&apos;s Biggest &amp; Best Rewards Club!
-            </p>
-
+            <FooterAuth />
             <LoginForm />
           </div>
           <FooterAuth />
